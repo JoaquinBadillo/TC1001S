fca88fd554a10e7433711c151d5934b532fb6f8d
SHAUL-ZA <shaulzayat1@gmail.com>
2022-03-24 11:37:21 -0600

    Change speed cannon

--- Python Games/cannon.py
+++ Python Games/cannon.py
@@ -22,8 +22,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 200) / 15
+        speed.y = (y + 200) / 15
 
 def inside(xy):
     "Return True if xy within screen."
